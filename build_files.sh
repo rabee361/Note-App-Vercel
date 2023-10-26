c30df46e80b65606de72daadb154213d789bfb37
rabee361 <rha60540@gmail.com>
2023-10-26 23:09:38 +0200

    changes

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,5 @@ pip3 install --upgrade setuptools
 pip install -r requirements.txt
 
 # make migrations
-python3.x manage.py migrate 
-python3.x manage.py collectstatic
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
