c5feb0135cb0b0630ff56d3889f552e6b305827b
rabee361 <rha60540@gmail.com>
2023-12-10 02:13:00 +0300

    changes

--- noteproject/settings.py
+++ noteproject/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'noteapp'
+    'noteapp',
     'corsheaders',
     'rest_framework'
 ]
